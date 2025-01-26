5eb609aef64fdab00590007258ef30c94fc80e1c
onemore-creator <alextiulip@gmail.com>
2025-01-27 00:57:02 +0100

    feat: parallel execution change, 1 worker per color

--- src/execParallel.js
+++ src/execParallel.js
@@ -1,11 +1,24 @@
-const { getColors } = require('./utils/getColors')
+const Piscina = require('piscina')
+const path = require('path')
+
+const piscina = new Piscina({
+  filename: path.resolve(__dirname, './getColorsWorker.js')
+})
 
 async function execParallel (colorFlags, colorOrder) {
-  // Fetch all colors in parallel
-  const colors = await getColors(colorFlags, colorOrder, async (colors) =>
-    Promise.all(colors)
-  )
-  console.info(colors.map((color) => color.HEX))
+  try {
+    const filteredColors = colorOrder.filter((color) => colorFlags[color])
+
+    const tasks = filteredColors.map((color) =>
+      piscina.run({ color })
+    )
+
+    const results = await Promise.all(tasks)
+
+    console.info('HEX Colors:', results.map((result) => result.HEX))
+  } catch (error) {
+    console.error('Error during parallel execution:', error.message)
+  }
 }
 
 module.exports = { execParallel }
---
+++ src/getColorsWorker.js
@@ -0,0 +1,13 @@
+const { getColors } = require('./utils/getColors')
+
+module.exports = async function (task) {
+  const { color } = task
+
+  const colorArray = await getColors(
+    { [color]: true },
+    [color],
+    async (promises) => Promise.all(promises)
+  )
+
+  return colorArray[0] // Return the first (and only) color object
+}
